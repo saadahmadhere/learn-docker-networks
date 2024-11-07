57bf507c5163f53b4fcac89d38f1b384b0403462
saadahmadhere <saadahmad728@gmail.com>
2024-11-08 01:57:34 +0530

    feat: add conatainer name in the mongoURL

--- src/db.ts
+++ src/db.ts
@@ -1,23 +1,24 @@
 import mongoose, { Schema, model } from 'mongoose';
 
-const mongoUrl: string = 'mongodb://mongo:27017/myDatabase';
+const mongoUrl: string = 'mongodb://mongo-container:27017/myDatabase';
 
 // Connect to MongoDB
-mongoose.connect(mongoUrl)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+	.connect(mongoUrl)
+	.then(() => console.log('MongoDB connected'))
+	.catch((err) => console.error('MongoDB connection error:', err));
 
 // Define a User schema
 interface IUser {
-  name: string;
-  age: number;
-  email: string;
+	name: string;
+	age: number;
+	email: string;
 }
 
 const UserSchema: Schema = new Schema<IUser>({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-  email: { type: String, required: true }
+	name: { type: String, required: true },
+	age: { type: Number, required: true },
+	email: { type: String, required: true },
 });
 
 // Create a User model
